c9baf5361fcc4a7a10d80d988ef83b8617e29fae
Gabrielgamer <bielgabrielfabianoteixeira@gmail.com>
2024-08-19 20:30:23 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+function abas(){
+				const extras = document.getElementById('extras');
+				
+				if (extras.style.display === 'none' || extras.style.display === '' ){
+    extras.style.display = 'block';
+    }
+				else{
+								extras.style.display = 'none';
+				}
+}
